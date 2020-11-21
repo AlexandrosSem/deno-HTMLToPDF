fef4402e8ee2ad2463f2f42b70b8877cd259a4c2
AlexandrosTheGreat <alexandrossemelidis@gmail.com>
2020-11-21 16:42:18 +0200

    Serve the html using oak

--- src/common/Dependency.ts
+++ src/common/Dependency.ts
@@ -1,1 +1,3 @@
-export * from 'https://deno.land/std@0.76.0/http/server.ts';
+//import puppeteer from 'https://jspm.dev/puppeteer';
+import { Application, Router, send } from 'https://deno.land/x/oak/mod.ts';
+export { Application as OakApplication, Router as OakRouter, send as OakSend };
