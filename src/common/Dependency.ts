9c6576cbd1931e515b74074e615358164be7aca7
AlexandrosTheGreat <alexandrossemelidis@gmail.com>
2020-11-22 22:26:31 +0200

    Fetch html using the router middleware

--- src/common/Dependency.ts
+++ src/common/Dependency.ts
@@ -1,3 +1,5 @@
-//import puppeteer from 'https://jspm.dev/puppeteer';
+//import { createRequire } from 'https://deno.land/std/node/module.ts';
+//const require = createRequire(import.meta.url);
+//const Puppeteer = require('../puppeteer');
 import { Application, Router, send } from 'https://deno.land/x/oak/mod.ts';
 export { Application as OakApplication, Router as OakRouter, send as OakSend };
--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ const PORT = 8080;
 const PUBLIC_PATH = `${Deno.cwd()}/src/client/public`;
 const getFullPath = (pPath: string) => `${PUBLIC_PATH}/${pPath}`;
 const router = new OakRouter();
-const app = new OakApplication();
-app.use(router.routes());
-app.use(router.allowedMethods());
-app.use(async (context) => {
+router.get('/', async (context) => {
 	await OakSend(context, context.request.url.pathname, {
 		root: PUBLIC_PATH,
 		index: 'index.html',
 	});
 });
+const app = new OakApplication();
+app.use(router.routes());
+app.use(router.allowedMethods());
 await app.listen({ port: PORT });
