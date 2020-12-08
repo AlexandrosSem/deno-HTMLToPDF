bc4c480399dba6a59526f83bfb9b58afb8c5354d
Taulim <taulimtank@gmail.com>
2020-12-08 20:47:14 +0000

    Implementation of a bold / italic parser.
    It is still not perfect but is working with simple text.

--- src/server/MDParserArcsecond.ts
+++ src/server/MDParserArcsecond.ts
@@ -4,48 +4,130 @@ const {
 	sequenceOf,
 	str,
 	many,
+	many1,
 	digit,
 	letter,
 	char,
 	anyChar,
 	between,
 	possibly,
+	whitespace,
+	recursiveParser,
+	anyCharExcept,
 } = Arcsecond;
 
+/// debug
+const debugOutput = (type: string) => (x: any) => ({ type: type, value: x });
+
+/// Builders
+const buildParseHeader = (pNumber: number) => {
+	const header = '#'.repeat(pNumber);
+	return sequenceOf([str(`${header} `).map(() => header), parseText]).map(
+		debugOutput(`h${pNumber}`)
+	);
+};
+
+const buildBetween = (pParser: any) => between(pParser)(pParser);
+
+const buildParseBold = (pParser: any) =>
+	choice([
+		buildBetween(str('**'))(pParser),
+		buildBetween(str('__'))(pParser),
+	]);
+
+const buildParseItalic = (pParser: any) =>
+	choice([
+		buildBetween(char('*'))(pParser),
+		buildBetween(char('_'))(pParser),
+	]);
+
+// const buildParseText = (pException: any) =>
+// 	many1(
+// 		choice([
+// 			parseEscape,
+// 			letter,
+// 			digit,
+// 			whitespace,
+// 			...(pException ? [] : [anyCharExcept(pException)]),
+// 		])
+// 	)
+// 		.map((x: any) => x.join(''))
+// 		.map(debugOutput('text2'));
+
+/// Helpers
 const parseEscape = sequenceOf([char('\\'), anyChar]).map((x: any) => x[1]);
-const parseText = many(choice([parseEscape, anyChar])).map((x: any) =>
-	x.join('')
+const parseText = many1(choice([parseEscape, letter, digit, whitespace]))
+	.map((x: any) => x.join(''))
+	.map(debugOutput('text'));
+
+/// Parsers
+const _ParseH1 = buildParseHeader(1);
+const _ParseH2 = buildParseHeader(2);
+const _ParseH3 = buildParseHeader(3);
+const _ParseH4 = buildParseHeader(4);
+const _ParseH5 = buildParseHeader(5);
+const _ParseH6 = buildParseHeader(6);
+
+const _ParseItalic = buildParseItalic(
+	recursiveParser(() =>
+		many1(choice([_ParseBold, parseText])).map(debugOutput('italic'))
+	)
 );
-const _ParseHeader1 = sequenceOf([str('# ').map(() => '#'), parseText]);
-const _ParseHeader2 = sequenceOf([str('## ').map(() => '##'), parseText]);
-const _ParseHeader3 = sequenceOf([str('### ').map(() => '###'), parseText]);
-const _ParseHeader4 = sequenceOf([str('#### ').map(() => '####'), parseText]);
-const _ParseHeader5 = sequenceOf([str('##### ').map(() => '#####'), parseText]);
-const _ParseHeader6 = sequenceOf([
-	str('###### ').map(() => '######'),
+
+const _ParseBold = buildParseBold(
+	recursiveParser(() =>
+		many1(choice([_ParseItalic, parseText])).map(debugOutput('bold'))
+	)
+);
+
+const _MDParser = choice([
+	_ParseH6,
+	_ParseH5,
+	_ParseH4,
+	_ParseH3,
+	_ParseH2,
+	_ParseH1,
+	_ParseBold,
+	_ParseItalic,
 	parseText,
 ]);
 
-const _MDParser = choice([
-	_ParseHeader6,
-	_ParseHeader5,
-	_ParseHeader4,
-	_ParseHeader3,
-	_ParseHeader2,
-	_ParseHeader1,
-]);
-
-export const MDParser = _MDParser;
+const _FinalParser = many(_MDParser);
 
 /// Maybe using a promise is not the best options here,
 /// but it will allow to change the parse engine quite easy.
 export function RunParser(pText: string): Promise<object> {
 	return new Promise((resolve, reject) => {
 		try {
-			const parsed = _MDParser.run(pText);
+			const parsed = _FinalParser.run(pText);
 			resolve(parsed);
 		} catch (ex) {
 			reject(ex);
 		}
 	});
 }
+
+const testParse = (pString: string) => {
+	const parsed = _FinalParser.run(pString);
+	const lineLen = 50;
+	console.log(`${pString} ${'-'.repeat(lineLen - (pString.length + 1))}`);
+	console.log(JSON.stringify(parsed, void 0, '  '));
+	console.log(`${'-'.repeat(lineLen)}`);
+};
+
+testParse('# test');
+testParse('## test');
+testParse('### test');
+testParse('#### test');
+testParse('##### test');
+testParse('###### test');
+testParse('test test');
+testParse('*italic text*');
+testParse('_italic text_');
+testParse('**bold text**');
+testParse('__bold text__');
+testParse('*sometext \\* test*');
+testParse('*italic **bold** italic*');
+testParse('**bold *italic* bold**');
+// This parse is still a problem...
+testParse('__bold @ text__');
