da665f5552fdd1ad0fbf7d2743307b22bf832e90
Taulim <taulimtank@gmail.com>
2020-12-10 19:28:24 +0000

    Better parser for Emphasis and nested parsers

--- src/server/MDParserArcsecond.ts
+++ src/server/MDParserArcsecond.ts
@@ -14,15 +14,45 @@ const {
 	whitespace,
 	recursiveParser,
 	anyCharExcept,
+	everyCharUntil,
+	skip,
+	pipeParsers,
 } = Arcsecond;
 
 /// debug
 const debugOutput = (type: string) => (x: any) => ({ type: type, value: x });
 
+/// Tokens
+interface iTOKEN {
+	H1: any;
+	H2: any;
+	H3: any;
+	H4: any;
+	H5: any;
+	H6: any;
+	Bold1: any;
+	Bold2: any;
+	Italic1: any;
+	Italic2: any;
+}
+
+const TOKEN: iTOKEN = {
+	H1: char('#'),
+	H2: str('##'),
+	H3: str('###'),
+	H4: str('####'),
+	H5: str('#####'),
+	H6: str('######'),
+	Bold1: str('**'),
+	Bold2: str('__'),
+	Italic1: char('*'),
+	Italic2: char('_'),
+};
+
 /// Builders
 const buildParseHeader = (pNumber: number) => {
-	const header = '#'.repeat(pNumber);
-	return sequenceOf([str(`${header} `).map(() => header), parseText]).map(
+	const parser = TOKEN[`H${pNumber}` as keyof iTOKEN];
+	return pipeParsers([parser, char(' '), parseText]).map(
 		debugOutput(`h${pNumber}`)
 	);
 };
@@ -31,32 +61,34 @@ const buildBetween = (pParser: any) => between(pParser)(pParser);
 
 const buildParseBold = (pParser: any) =>
 	choice([
-		buildBetween(str('**'))(pParser),
-		buildBetween(str('__'))(pParser),
+		buildBetween(TOKEN.Bold1)(pParser),
+		buildBetween(TOKEN.Bold2)(pParser),
 	]);
 
 const buildParseItalic = (pParser: any) =>
 	choice([
-		buildBetween(char('*'))(pParser),
-		buildBetween(char('_'))(pParser),
+		buildBetween(TOKEN.Italic1)(pParser),
+		buildBetween(TOKEN.Italic2)(pParser),
 	]);
 
-// const buildParseText = (pException: any) =>
-// 	many1(
-// 		choice([
-// 			parseEscape,
-// 			letter,
-// 			digit,
-// 			whitespace,
-// 			...(pException ? [] : [anyCharExcept(pException)]),
-// 		])
-// 	)
-// 		.map((x: any) => x.join(''))
-// 		.map(debugOutput('text2'));
-
 /// Helpers
+const parseTokens = choice([
+	TOKEN.H6,
+	TOKEN.H5,
+	TOKEN.H4,
+	TOKEN.H3,
+	TOKEN.H2,
+	TOKEN.H1,
+	TOKEN.Bold1,
+	TOKEN.Bold2,
+	TOKEN.Italic1,
+	TOKEN.Italic2,
+]);
 const parseEscape = sequenceOf([char('\\'), anyChar]).map((x: any) => x[1]);
-const parseText = many1(choice([parseEscape, letter, digit, whitespace]))
+const parseRawText = many1(anyChar)
+	.map((x: any) => x.join(''))
+	.map(debugOutput('rawText'));
+const parseText = many1(choice([parseEscape, anyCharExcept(parseTokens)]))
 	.map((x: any) => x.join(''))
 	.map(debugOutput('text'));
 
@@ -90,9 +122,10 @@ const _MDParser = choice([
 	_ParseBold,
 	_ParseItalic,
 	parseText,
+	parseRawText,
 ]);
 
-const _FinalParser = many(_MDParser);
+const _FinalParser = many1(_MDParser);
 
 /// Maybe using a promise is not the best options here,
 /// but it will allow to change the parse engine quite easy.
@@ -115,13 +148,13 @@ const testParse = (pString: string) => {
 	console.log(`${'-'.repeat(lineLen)}`);
 };
 
-testParse('# test');
-testParse('## test');
-testParse('### test');
-testParse('#### test');
-testParse('##### test');
-testParse('###### test');
-testParse('test test');
+testParse('# Header 1');
+testParse('## Header 2');
+testParse('### Header 3');
+testParse('#### Header 4');
+testParse('##### Header 5');
+testParse('###### Header 6');
+testParse('Simple text');
 testParse('*italic text*');
 testParse('_italic text_');
 testParse('**bold text**');
@@ -129,5 +162,13 @@ testParse('__bold text__');
 testParse('*sometext \\* test*');
 testParse('*italic **bold** italic*');
 testParse('**bold *italic* bold**');
-// This parse is still a problem...
 testParse('__bold @ text__');
+testParse('_italic @ text_');
+testParse('__bold ~ text__');
+testParse('_italic £ text_');
+testParse('_italic \\* text_');
+testParse('_italic \\_ text_');
+testParse('#test');
+testParse('test #test');
+// This parse is still a problem...
+testParse('_test #test_');
